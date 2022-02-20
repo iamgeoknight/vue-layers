5818801fecb2e9e4c3b1212b74a48e057bff42c7
iamgeoknight <pkchaudhary265@gmail.com>
2022-02-20 20:51:28 +0530

    Integrating OpenLayers Map in VueJS

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <Map></Map>
+</template>
+
+<script>
+import Map from './components/Map.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Map
+  }
+}
+</script>
+
+<style>
+@import url('../node_modules/ol/src/ol.css');
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+html, body, #map, #app {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/Map.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id = "map">
+    </div>
+</template>
+
+<script>
+import { Map, View } from 'ol';
+import { Tile as TileLayer } from 'ol/layer';
+import { OSM } from 'ol/source';
+
+export default {
+    name: 'Map',
+    mounted() {
+        this.map = new Map({
+            target: 'map',
+            layers: [
+                new TileLayer({
+                    source: new OSM()
+                })
+            ],
+            view: new View({
+                center: [0, 0],
+                zoom: 2
+            })
+        })
+    }
+}
+</script>
